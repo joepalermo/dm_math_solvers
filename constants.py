5b8ebd2577948c4a541032c9816b61d9fc506bae
Joseph Palermo <jp@deeplearni.ng>
2020-12-06 14:51:46 -0500

    cleanup

--- constants.py
+++
@@ -1,5 +0,0 @@
-from preprocessing import char2idx
-
-vocab_size = len(char2idx)  # + 2  # +1 for the padding token, which is 0, and +1 for the delimiter newline/start token
-question_max_length = 160
-answer_max_length = 30
