227e7cb859d3aa4c9156282f85c28c091ee4d1f9
Joseph Palermo <jpalermo@squareup.com>
2020-12-09 14:44:37 -0500

    all unit tests pass

--- environment/unit_testing/test_compute_graph.py
+++ environment/unit_testing/test_compute_graph.py
@@ -11,9 +11,9 @@ from environment.compute_graph import ComputeGraph, Node
 class Test(unittest.TestCase):
 
     def test_easy_algebra__linear_1d(self):
-        problem_statement = 'Solve $f[0 = 4*b + b + 15] for $f[b].'
+        problem_statement = 'Solve 0 = 4*b + b + 15 for b.'
         f = extract_formal_elements(problem_statement)
-        cg = ComputeGraph()
+        cg = ComputeGraph(problem_statement)
         lookup_value_node = Node(lookup_value)
         solve_system_node = Node(solve_system)
         solve_system_node.set_arg(f[0])
@@ -23,7 +23,8 @@ class Test(unittest.TestCase):
         assert cg.eval() == -3
 
     def test_incomplete_compute_graph(self):
-        cg = ComputeGraph()
+        problem_statement = 'Solve 0 = 4*b + b + 15 for b.'
+        cg = ComputeGraph(problem_statement)
         lookup_value_node = Node(lookup_value)
         solve_system_node = Node(solve_system)
         lookup_value_node.set_arg(solve_system_node)
@@ -31,13 +32,3 @@ class Test(unittest.TestCase):
         assert str(cg) == "lookup_value(solve_system('param_0'),'param_1')"
         assert cg.eval() == None
 
-
-    def test_easy_algebra__linear_1d_composed(self):
-        problem_statement = 'Let $f[w] be $f[(-1 + 13)*3/(-6)]. Let $f[b = w - -6]. Let $f[i = 2 - b]. Solve $f[-15 = 3*c + i*c] for $f[c].'
-        f = extract_formal_elements(problem_statement)
-        # eq1 = make_equality(f[0], f[1])
-        # system = append(append(append(None, eq1), f[2]), f[3])
-        # soln = solve_system(system)
-        # i_eq = lookup_value_eq(soln, project_lhs(f[3]))
-        # lin_eq = substitution_left_to_right(f[4], i_eq)
-        # assert lookup_value(solve_system(lin_eq), f[5]) == -3
