a7ab76cf74b4db95e9bb4482e8201184952fb305
Joseph Palermo <jpalermo@squareup.com>
2020-12-09 14:15:28 -0500

    adding code to automatically extract annotations

---
+++ preprocessing/unit_testing/test_extract_formal_elements.py
@@ -0,0 +1,14 @@
+import unittest
+from utils import read_json, write_json
+from preprocessing import extract_formal_elements
+
+def run_test_case(question, formal_elements):
+    extracted_formal_elements = extract_formal_elements(question)
+    assert extracted_formal_elements == formal_elements
+
+class Test(unittest.TestCase):
+
+    def test_examples(self):
+        question_to_formal_elements = read_json('preprocessing/unit_testing/extract_formal_elements_examples.json')
+        for question, formal_elements in question_to_formal_elements.items():
+            run_test_case(question, formal_elements)
