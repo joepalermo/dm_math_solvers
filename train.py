fbafe723aa72d1602f75ebde3f23788aea113e96
Alok Singh <alokbeniwal@gmail.com>
2020-12-17 11:35:18 -0800

    change starter code

--- train.py
+++ train.py
@@ -1,27 +1,112 @@
-import tensorflow as tf
-import numpy as np
-import os
+#!/usr/bin/env python3
+
 import argparse
-from utils import get_logger
-from preprocessing import load_train, build_train_and_val_datasets
-from transformer.params import TransformerParams
-from transformer.transformer import Transformer
+import functools
+import itertools
+import logging
+import os
+import random
+import re
+import shutil
+import subprocess
+import sys
+import typing
+from copy import deepcopy
+from functools import reduce
+from logging import debug, info, log
+from pathlib import Path
 
-np.random.seed(1234)
-tf.random.set_seed(1234)
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--eager', metavar='eager_mode', type=bool, default=True, help='Eager mode on, else Autograph')
-parser.add_argument('--gpu_id', metavar='gpu_id', type=str, default="1", help='The selected GPU to use, default 1')
-args = parser.parse_args()
-tf.config.experimental_run_functions_eagerly(args.eager)
-params = TransformerParams()
-os.environ['CUDA_VISIBLE_DEVICES'] = params.gpu_id
+import gym
+import numpy as np
+import ray
+from gym.spaces import Box, Discrete
+from ray import tune
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.tf.fcnet import FullyConnectedNetwork
+from ray.rllib.models.tf.tf_modelv2 import TFModelV2
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.utils.framework import try_import_tf, try_import_torch
+from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.tune import grid_search
 
-if __name__ == '__main__':
-    logger = get_logger('validation', params.experiment_dirpath)
-    logger.info("Logging to {}".format(params.experiment_dirpath))
-    module_name_to_arrays = load_train('easy', num_files_to_include=10)
-    train_ds, module_name_to_val_ds = build_train_and_val_datasets(module_name_to_arrays, TransformerParams())
-    model = Transformer(params)
-    model.load_latest_checkpoint()
-    model.train(params, train_ds, module_name_to_val_ds, logger)
+from environment.envs.math_env import MathEnv
+
+"""Example of a custom gym environment and model. Run this for a demo.
+
+This example shows:
+  - using a custom environment
+  - using a custom model
+  - using Tune for grid search
+
+You can visualize experiment results in ~/ray_results using TensorBoard.
+"""
+
+torch, nn = try_import_torch()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--run", type=str, default="PPO")
+parser.add_argument("--torch", action="store_true")
+parser.add_argument("--as-test", action="store_true")
+parser.add_argument("--stop-iters", type=int, default=50)
+parser.add_argument("--stop-timesteps", type=int, default=100000)
+parser.add_argument("--stop-reward", type=float, default=0.1)
+
+# TODO mathenv needs to take in dict `config` in __init__` (for ray to work
+
+
+class TorchCustomModel(TorchModelV2, nn.Module):
+    """Example of a PyTorch custom model that just delegates to a fc-net."""
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        TorchModelV2.__init__(
+            self, obs_space, action_space, num_outputs, model_config, name
+        )
+        nn.Module.__init__(self)
+
+        self.torch_sub_model = TorchFC(
+            obs_space, action_space, num_outputs, model_config, name
+        )
+
+    def forward(self, input_dict, state, seq_lens):
+        input_dict["obs"] = input_dict["obs"].float()
+        fc_out, _ = self.torch_sub_model(input_dict, state, seq_lens)
+        return fc_out, []
+
+    def value_function(self):
+        return torch.reshape(self.torch_sub_model.value_function(), [-1])
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    ray.init()
+
+    # Can also register the env creator function explicitly with:
+    # register_env("corridor", lambda config: SimpleCorridor(config))
+    ModelCatalog.register_custom_model("my_model", TorchCustomModel)
+
+    config = {
+        "env": MathEnv,  # or "corridor" if registered above
+        "env_config": {...},  # TODO
+        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+        "num_gpus": torch.cuda.device_count(),
+        "model": {
+            "custom_model": "my_model",
+        },
+        "vf_share_layers": True,
+        "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
+        "num_workers": 1,  # parallelism
+        "framework": "torch",
+    }
+
+    stop = {
+        "training_iteration": args.stop_iters,
+        "timesteps_total": args.stop_timesteps,
+        "episode_reward_mean": args.stop_reward,
+    }
+
+    results = tune.run(args.run, config=config, stop=stop)
+
+    if args.as_test:
+        check_learning_achieved(results, args.stop_reward)
+    ray.shutdown()
