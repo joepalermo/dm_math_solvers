77796dc07d2c91eeacf0682ee1ccdb01d927322d
Joseph Palermo <jp@deeplearni.ng>
2020-12-18 11:13:41 -0500

    add num_transformer_units: 1,

--- train.py
+++ train.py
@@ -28,6 +28,7 @@ from ray.rllib.models.tf.fcnet import FullyConnectedNetwork
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.tf.attention_net import GTrXLNet
 from ray.rllib.utils.framework import try_import_tf, try_import_torch
 from ray.rllib.utils.test_utils import check_learning_achieved
 from ray.tune import grid_search
@@ -59,10 +60,22 @@ parser.add_argument("--stop-reward", type=float, default=0.1)
 
 # TODO mathenv needs to take in dict `config` in __init__` (for ray to work
 
+# "model": {
+#     "custom_model": GTrXLNet,
+#     "max_seq_len": 50,
+#     "custom_model_config": {
+#         "num_transformer_units": 1,
+#         "attn_dim": 64,
+#         "num_heads": 2,
+#         "memory_tau": 50,
+#         "head_dim": 32,
+#         "ff_hidden_dim": 32,
+#     },
+# },
 
 # TODO lightning module
 class TorchCustomModel(TorchModelV2, nn.Module):
-    """Example of a PyTorch custom model that just delegates to a fc-net."""
+    """Example of a PyTorch custom model that just delegates to a GTrXLNet."""
 
     def __init__(
         self,
@@ -82,14 +95,19 @@ class TorchCustomModel(TorchModelV2, nn.Module):
         )
         nn.Module.__init__(self)
 
-        self.torch_sub_model = TorchFC(
-            obs_space, action_space, num_outputs, model_config, name
-        )
+        self.torch_sub_model = GTrXLNet(
+            obs_space, action_space, num_outputs, model_config, name,
+            num_transformer_units=1,
+            attn_dim=64,
+            num_heads=2,
+            memory_tau=50,
+            head_dim=32,
+            ff_hidden_dim=32)
 
     def forward(self, input_dict, state, seq_lens):
         input_dict["obs"] = input_dict["obs"].float()
-        fc_out, _ = self.torch_sub_model(input_dict, state, seq_lens)
-        return fc_out, []
+        logits, _ = self.torch_sub_model(input_dict, state, seq_lens)
+        return logits, []
 
     def value_function(self):
         return torch.reshape(self.torch_sub_model.value_function(), [-1])
