c346121afe02fb0ffc7cba57bb4a6b44ad951e25
Johnny Ye <jye@squareup.com>
2021-01-22 12:47:15 -0500

    finished rl test

--- rl_unit_test.py
+++ rl_unit_test.py
@@ -2,11 +2,36 @@ import gym
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from hparams import HParams
+hparams = HParams('.', hparams_filename='hparams', name='rl_math', ask_before_deletion=False)
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from modelling.train_utils import train_on_buffer, StepDataset, get_logdir
+from utils import flatten
+from modelling.train_utils import train_on_buffer, get_logdir
 device = torch.device(f'cuda:{0}' if torch.cuda.is_available() else 'cpu')
 writer = SummaryWriter(log_dir=get_logdir())
+import numpy as np
+
+
+class StepDataset(torch.utils.data.Dataset):
+    """Step Dataset"""
+
+    def __init__(self, trajectory_buffer, device):
+        self.step_buffer = flatten(trajectory_buffer)
+        self.device = device
+
+    def __len__(self):
+        return len(self.step_buffer)
+
+    def __getitem__(self, idx):
+        # return only the (state, action, reward)?
+        state, action, reward, next_state, done = self.step_buffer[idx]
+        state = torch.from_numpy(state.astype(np.float32)).to(self.device)
+        action = torch.from_numpy(np.array(action, dtype=np.int64)).to(self.device)
+        reward = torch.from_numpy(np.array(reward, dtype=np.float32)).to(self.device)
+        next_state = torch.from_numpy(next_state.astype(np.float32)).to(self.device)
+        done = torch.from_numpy(np.array(done, dtype=np.int64)).to(self.device)
+        return state, action, reward, next_state, done
 
 
 def generate_trajectories(num_episodes=20, episode_timeout=100):
@@ -16,16 +41,41 @@ def generate_trajectories(num_episodes=20, episode_timeout=100):
         state = env.reset()
         trajectory = []
         for t in range(episode_timeout):
-            env.render()
-            action = env.action_space.sample()
+            #env.render()
+            torch_state = torch.from_numpy(state.astype(np.float32)).to(model.device)
+            if np.random.random() < 0.1:
+                action = env.action_space.sample()
+            else:
+                action = torch.argmax(model(torch_state)).item()
             next_state, reward, done, info = env.step(action)
             trajectory.append((state, action, reward, next_state, done))
+            state = next_state
             if done:
-                print("Episode finished after {} timesteps".format(t+1))
+                trajectory[-1] = (trajectory[-1][0], trajectory[-1][1], -1, trajectory[-1][3], trajectory[-1][4])
+                #print("Episode finished after {} timesteps".format(t+1))
                 trajectories.append(trajectory)
                 break
     env.close()
     return trajectories
+
+def run_eval(model, num_episodes=200, episode_timeout=100):
+    env = gym.make('CartPole-v0')
+    rewards = list()
+    for i_episode in range(num_episodes):
+        tot_reward = 0
+        state = env.reset()
+        for t in range(episode_timeout):
+            #env.render()
+            state = torch.from_numpy(state.astype(np.float32)).to(model.device)
+            action = torch.argmax(model(state))
+            state, reward, done, info = env.step(action.item())
+            tot_reward += reward
+            if done:
+                #print("Episode finished after {} timesteps".format(t+1))
+                break
+        rewards.append(tot_reward)
+    env.close()
+    print("Mean Reward per episode:", sum(rewards)/len(rewards))
 
 
 class MLP(nn.Module):
@@ -33,27 +83,29 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.device = device
         self.layers = nn.Sequential(
-            nn.Linear(784, 100),
+            nn.Linear(4, 10),
             nn.ReLU(),
-            nn.Linear(100, 10)
+            nn.Linear(10, 2)
         )
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        self.batch_size = 32
 
     def forward(self, x):
-        # convert tensor (128, 1, 28, 28) --> (128, 1*28*28)
-        # x = x.view(x.size(0), -1)
         x = self.layers(x)
         return x
 
 
 model = MLP(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 loss_fn = nn.CrossEntropyLoss()
 
 
 batch_size = 32
-batches_per_train = 10
+batches_per_train = 5
+batches_per_eval = 10
+last_eval_batch_i = 0
+batch_i = 0
 
-for _ in range(1):
+for _ in range(1000):
     # gather data
     trajectories = generate_trajectories()
     step_dataset = StepDataset(trajectories, model.device)
@@ -63,3 +115,6 @@ for _ in range(1):
     batches_to_train = min(batches_in_dataset, batches_per_train)
     batch_i = train_on_buffer(model, data_loader, batches_to_train, writer, batch_i)
     # eval
+    if batch_i - last_eval_batch_i >= batches_per_eval:
+        last_eval_batch_i = batch_i
+        run_eval(model)
