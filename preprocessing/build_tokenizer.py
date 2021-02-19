295d393155bfcb3599ef9be1b1f8f6800576e6fb
Joseph Palermo <jpalermo@squareup.com>
2021-02-19 11:03:28 -0500

    update tokenizer to leave out the text representation of the compute graph

--- preprocessing/build_tokenizer.py
+++ preprocessing/build_tokenizer.py
@@ -1,26 +1,28 @@
-from hparams import HParams
-hparams = HParams('.', hparams_filename='hparams', name='rl_math', ask_before_deletion=False)
+# from hparams import HParams
+# hparams = HParams('.', hparams_filename='hparams', name='rl_math', ask_before_deletion=False)
 import sentencepiece as spm
 import matplotlib.pyplot as plt
 from utils import read_text_file
 
 # train tokenizer on question corpus
-hardcoded_symbols = hparams.env.operators + hparams.env.types + ["'p_0'", "'p_1'", "'", 'G']  # why is 'G' needed?
-spm.SentencePieceTrainer.train(input='environment/tokenization/question_corpus.txt',
+# hardcoded_symbols = hparams.env.operators + hparams.env.types + ["'p_0'", "'p_1'", "'", 'G']  # why is 'G' needed?
+hardcoded_symbols = ['G']  # why is 'G' needed?
+spm.SentencePieceTrainer.train(input='environment/tokenization/train_question_corpus.txt',
                                model_prefix='environment/tokenization/tokenizer',
                                vocab_size=250,
                                user_defined_symbols=hardcoded_symbols)
 
+
 # load tokenizer
 sp = spm.SentencePieceProcessor(model_file='environment/tokenization/tokenizer.model')
-example_state = "What is the third derivative of -t**4 - 880*t**3 + 152*t**2 wrt t?; differentiate('p_0')"
+example_state = "What is the third derivative of -t**4 - 880*t**3 + 152*t**2 wrt t?"
 id_tokens = sp.encode(example_state)
 string_tokens = sp.encode(example_state, out_type=str)
 assert sp.decode(id_tokens) == example_state, sp.decode(id_tokens)
 assert sp.decode(string_tokens) == example_state, sp.decode(string_tokens)
 
 # visualize encoded lengths using trained tokenizer on graph corpus
-graph_corpus = read_text_file('environment/tokenization/graph_corpus.txt')
+graph_corpus = read_text_file('environment/tokenization/val_question_corpus.txt')
 raw_observations = graph_corpus.split('\n')
 for raw_obs in raw_observations:
     assert raw_obs == sp.decode(sp.encode(raw_obs))
--- preprocessing/collect_question_corpus.py
+++ preprocessing/collect_question_corpus.py
@@ -1,14 +1,13 @@
-from hparams import HParams
-hparams = HParams('.', hparams_filename='hparams', name='rl_math', ask_before_deletion=False)
 from random import shuffle
 from utils import read_text_file
+from sklearn.model_selection import train_test_split
 
 filenames = read_text_file("environment/module_lists/most_natural_composed_for_program_synthesis.txt").split("\n")
 filepaths = [
     f"mathematics_dataset-v1.0/train-easy/{filename}" for filename in filenames
 ]
 
-num_problems = 50000
+num_problems = 250000
 num_problems_per_module = num_problems // len(filepaths)
 questions = []
 
@@ -19,8 +18,11 @@ for filepath in filepaths:
     for i in range(0, 2 * num_pairs, 2):
         question = lines[i].strip()
         answer = lines[i + 1].strip()
-        questions.append(question + ';')
+        questions.append(question)
 
 shuffle(questions)
-with open("environment/tokenization/question_corpus.txt", "w") as f:
-    f.write("\n".join(questions))
+train_questions, val_questions = train_test_split(questions, test_size=0.4)
+with open("environment/tokenization/train_question_corpus.txt", "w") as f:
+    f.write("\n".join(train_questions))
+with open("environment/tokenization/val_question_corpus.txt", "w") as f:
+    f.write("\n".join(val_questions))
